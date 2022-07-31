6850695c3e3b1ef32ad62726ca231ac8cdbc5b11
nbogie <neillbogie@googlemail.com>
2022-07-31 01:53:38 +0100

    basic display and update of kata progress

---
+++ src/KataProgressApp.tsx
@@ -0,0 +1,184 @@
+import {
+    Box,
+    Checkbox, Input, Link, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr, useToast
+} from "@chakra-ui/react";
+
+import { useEffect, useState } from "react";
+import axios from "axios"
+interface Kata {
+    id: string;
+    url: string;
+    name: string;
+    status?: string;
+    difficulty?: string;
+    createdTime?: string;
+    lastEditedTime?: string;
+}
+interface DecoratedKata {
+    kata: Kata;
+    progress?: KataProgress;
+}
+interface KataProgress {
+    is_done: boolean;
+    is_stuck: boolean;
+}
+interface KataProgressData {
+    kata_id: string;
+    user_id: string;
+    is_done: boolean;
+    is_stuck: boolean;
+}
+interface User {
+    id: string;
+}
+export function KataProgressApp() {
+    const [user, setUser] = useState<User>({ id: "1" });
+    const [katas, setKatas] = useState<Kata[]>([]);
+    const [katasProgressData, setKatasProgressData] = useState<KataProgressData[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const toast = useToast()
+
+    useEffect(() => {
+        async function fetchAndSaveKatas() {
+            const katasRes = await fetch("http://localhost:4000/katas");
+            const katasData = await katasRes.json();
+            const progressRes = await fetch(`http://localhost:4000/user/${user.id}/kata_progress`);
+            const progressData = await progressRes.json();
+            setKatas(katasData.data.katas);
+            setKatasProgressData(progressData.data);
+        }
+        fetchAndSaveKatas();
+    }, [user]);
+
+    const decoratedKatas: DecoratedKata[] = calcDecoratedKatas();
+    function calcDecoratedKatas() {
+        console.log({ katasProgressData })
+        return katas.map(k => {
+            const progress = katasProgressData.find(pd => pd.kata_id === k.id)
+            const dk: DecoratedKata = { kata: k, progress }
+            dk.progress = progress;
+            return dk;
+        })
+    }
+
+    async function updateStatusOnKata(user_id: string, kata_id: string, newStatus: boolean) {
+        console.log(`would update status on kata ${kata_id} for user: ${user_id}`)
+        try {
+            const res = await axios.post(`http://localhost:4000/user/${user_id}/kata/${kata_id}/progress`, {
+                user_id: user_id,
+                kata_id: kata_id,
+                is_done: newStatus
+            })
+            if (res.status >= 200 && res.status < 400) {
+                toast({
+                    title: 'Updated kata status',
+                    description: "Now set to " + (newStatus ? "completed" : "not completed"),
+                    status: 'success',
+                    duration: 2000,
+                    isClosable: true,
+                })
+
+            }
+        } catch (err) {
+
+            toast({
+                title: 'Problem updating kata status',
+                description: "Error communicating with server - Please retry later",
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            })
+
+        }
+
+    }
+
+    function sortDecoratedKatas(ks: DecoratedKata[]): DecoratedKata[] {
+        function compareTitles(a: DecoratedKata, b: DecoratedKata): -1 | 1 | 0 {
+            const an = a.kata.name.toLowerCase();
+            const bn = b.kata.name.toLowerCase();
+            if (an < bn) {
+                return -1;
+            }
+            if (an > bn) {
+                return 1;
+            }
+            return 0;
+        }
+        function compareDifficulties(a: DecoratedKata, b: DecoratedKata): -1 | 1 | 0 {
+            const da = parseInt((a.kata.difficulty ?? "0").split(" ")[0]);
+            const db = parseInt((b.kata.difficulty ?? "0").split(" ")[0]);
+
+            if (da < db) {
+                return -1;
+            }
+            if (da > db) {
+                return 1;
+            }
+            return compareTitles(a, b)
+        }
+        return [...ks].sort(compareDifficulties)
+
+    }
+    const decoratedKatasToShow = sortDecoratedKatas(decoratedKatas.filter(k => k.kata.status !== "not ready").filter(k => k.kata.name.toLowerCase().includes(searchTerm.toLowerCase())))
+
+
+    return (
+        <Box>
+            <Input
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+                placeholder='search here...'
+                size='sm'
+            />
+            <TableContainer>
+                <Table >
+                    <TableCaption>Kata Progress</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>difficulty</Th>
+                            <Th>status</Th>
+                            <Th>My progress</Th>
+                            <Th>Kata Title</Th>
+
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {decoratedKatasToShow.map(k => (
+                            <Tr key={k.kata.id}>
+
+                                <Td>
+                                    {k.kata.difficulty}
+                                </Td>
+                                <Td>
+                                    {k.kata.status}
+                                </Td>
+                                <Td>
+                                    <Checkbox
+                                        onChange={(e) => updateStatusOnKata(user.id, k.kata.id, e.target.checked)}
+                                        isChecked={k.progress?.is_done}
+                                    >done</Checkbox>
+                                </Td>
+                                <Td>
+                                    <Link
+                                        color="teal.500"
+                                        href={k.kata.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >{k.kata.name}
+                                    </Link>
+                                </Td>
+                            </Tr>
+
+
+                        ))}
+
+                    </Tbody>
+                </Table>
+            </TableContainer>
+
+        </Box >
+    )
+}
+
