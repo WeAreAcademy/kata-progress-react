db99921ef669da6445c2d553502d1fd615dbcdf9
nbogie <neillbogie@googlemail.com>
2022-07-31 11:41:31 +0100

    minor refactor of a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
-import React from "react"
 import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
 import { App } from "./App"
+import { render } from "./test-utils"
 
 test("renders Kata Progress link", () => {
   render(<App />)
-  const linkElement = screen.getByText(/Kata Progress/i)
-  expect(linkElement).toBeInTheDocument()
+  const el = screen.getByText(/Kata Progress/i)
+  expect(el).toBeInTheDocument()
 })
