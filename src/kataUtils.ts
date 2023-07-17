e58d5b80dd35bb41922140cd4729b9c4b336e5bd
nbogie <neillbogie@googlemail.com>
2023-07-17 11:39:35 +0100

    formatting only

--- src/kataUtils.ts
+++ src/kataUtils.ts
@@ -1,28 +1,31 @@
 import { DecoratedKata } from "./types";
 
 export function sortDecoratedKatas(ks: DecoratedKata[]): DecoratedKata[] {
-  function compareTitles(a: DecoratedKata, b: DecoratedKata): -1 | 1 | 0 {
-    const an = a.kata.name.toLowerCase();
-    const bn = b.kata.name.toLowerCase();
-    if (an < bn) {
-      return -1;
+    function compareTitles(a: DecoratedKata, b: DecoratedKata): -1 | 1 | 0 {
+        const an = a.kata.name.toLowerCase();
+        const bn = b.kata.name.toLowerCase();
+        if (an < bn) {
+            return -1;
+        }
+        if (an > bn) {
+            return 1;
+        }
+        return 0;
     }
-    if (an > bn) {
-      return 1;
-    }
-    return 0;
-  }
-  function compareDifficulties(a: DecoratedKata, b: DecoratedKata): -1 | 1 | 0 {
-    const da = parseInt((a.kata.difficulty ?? "0").split(" ")[0]);
-    const db = parseInt((b.kata.difficulty ?? "0").split(" ")[0]);
+    function compareDifficulties(
+        a: DecoratedKata,
+        b: DecoratedKata
+    ): -1 | 1 | 0 {
+        const da = parseInt((a.kata.difficulty ?? "0").split(" ")[0]);
+        const db = parseInt((b.kata.difficulty ?? "0").split(" ")[0]);
 
-    if (da < db) {
-      return -1;
+        if (da < db) {
+            return -1;
+        }
+        if (da > db) {
+            return 1;
+        }
+        return compareTitles(a, b);
     }
-    if (da > db) {
-      return 1;
-    }
-    return compareTitles(a, b);
-  }
-  return [...ks].sort(compareDifficulties);
+    return [...ks].sort(compareDifficulties);
 }
